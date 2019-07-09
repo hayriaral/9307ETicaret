03201877f22c01c233b6fb4a08e53f5e7a47ec88
Hayri <hayriaral@gmail.com>
2019-07-09 21:14:26 +0300

    updated.

--- 9307ETicaret/ETicaret.Common/Tool.cs
+++ 9307ETicaret/ETicaret.Common/Tool.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ETicaret.Entity;
 
 namespace ETicaret.Common
 {
     public class Tool
     {
+        public static ECommerceEntities db = null;
+        public static ECommerceEntities GetConnection()
+        {
+            if (db == null)
+            {
+                db = new ECommerceEntities();
+            }
+            return db;
+        }
     }
 }
