6ac72eb3321160ab53876dee127eba65372fffb3
Hayri <hayriaral@gmail.com>
2019-07-10 21:04:19 +0300

    Updated 10.07.2019

---
+++ 9307ETicaret/ETicaret.Repository/CategoryRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaret.Entity;
+using ETicaret.Common;
+
+namespace ETicaret.Repository
+{
+    //uniqueidentifier karşılığı burda Guiddir.
+    class CategoryRepository : DataRepository<Category, Guid>
+        //DataRepository ctrl . ile abstracları aktarıyoruz.
+    {
+        static ECommerceEntities db = Tool.GetConnection();
+        ResultProcess<Category> result = new ResultProcess<Category>();
+
+        public override Result<int> Delete(Guid id)
+        {
+            Category cat = db.Categories.SingleOrDefault(t => t.CategoryID == id);
+            db.Categories.Remove(cat);
+            return result.GetResult(db);
+        }
+
+        public override Result<Category> GetObjByID(Guid id)
+        {
+            Category cat = db.Categories.SingleOrDefault(t => t.CategoryID == id);
+            return result.GetT(cat);
+        }
+
+        public override Result<int> Insert(Category item)
+        {
+            Category newCat = db.Categories.Create();
+            newCat.CategoryName = item.CategoryName;
+            newCat.CreatedDate = DateTime.Now;
+            newCat.Description = item.Description;
+
+            db.Categories.Add(newCat);
+            return result.GetResult(db);
+        }
+
+        public override Result<List<Category>> List()
+        {
+            List<Category> CatList = db.Categories.ToList();
+            return result.GetListResult(CatList);
+        }
+
+        public override Result<int> Update(Category item)
+        {
+            Category dbdeki = db.Categories.SingleOrDefault(t => t.CategoryID == item.CategoryID);
+
+            dbdeki.CategoryName = item.CategoryName;
+            dbdeki.Description = item.Description;
+
+            return result.GetResult(db);
+        }
+    }
+}
---
+++ 9307ETicaret/ETicaret.Repository/DataRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaret.Common;
+
+namespace ETicaret.Repository
+{
+    //
+    public abstract class DataRepository<T,M>
+    {
+        public abstract Result<int> Insert(T item);
+        public abstract Result<int> Update(T item);
+        public abstract Result<int> Delete(M id);
+        public abstract Result<List<T>> List();
+        public abstract Result<T> GetObjByID(M id);
+    }
+}
