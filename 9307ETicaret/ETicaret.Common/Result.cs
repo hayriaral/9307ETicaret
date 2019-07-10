96f5163d5ba8d567868e3ab9ae0737478966b071
Hayri <hayriaral@gmail.com>
2019-07-10 19:48:47 +0300

    Commondaki tüm classlar oluşturuldu.

---
+++ 9307ETicaret/ETicaret.Common/Result.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaret.Common
+{
+    //Bu class generic typeta bir classtır. Parametre değeri ne gelirse ona bürünür.
+    //Belli tasklar altında çalışır. Helper class olarak da bilinir.
+    //Tamamen yardımcı işler icin kullanılır.
+    public class Result<T>
+    {
+        public string UserMessage { get; set; }
+        public bool IsSucceeded { get; set; }
+        public T ProcessResult { get; set; } //Categori çekmek icin kullanacağız.
+    }
+}
