2ea2afe56dcfbd417948b602b968507fd3a9d385
Hayri <hayriaral@gmail.com>
2019-07-25 20:39:33 +0300

    HomeController completed.

--- 9307ETicaret/ETicaret.MVC/Controllers/HomeController.cs
+++ 9307ETicaret/ETicaret.MVC/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ETicaret.Repository;
+using ETicaret.Entity;
 
 namespace ETicaret.MVC.Controllers
 {
@@ -15,6 +16,37 @@ namespace ETicaret.MVC.Controllers
         public ActionResult Index()
         {
             return View(pr.GetLatestObjects(5).ProcessResult);
+        }
+
+        //Soldaki kategori menüsündeki kategori isimlerine tıklağımızda o kategorideki ürünleri getiren method.
+        public ActionResult GetProductByCatID(Guid id)
+        {
+            List<Product> ProList = ProductRepository.db.Products.Where(t => t.CategoryID == id).ToList();
+
+            //var PList = pr.List().ProcessResult.Where(t => t.CategoryID == id).ToList();
+            //2. yöntemde bütün productlar çekildikten sonra filtreleme yapılıyor. Bu sebeple işlemi daha yorucu.
+
+            return View(ProList);
+        }
+
+        public ActionResult GetProductByBrandID(int id)
+        {
+            List<Product> ProList = ProductRepository.db.Products.Where(t => t.BrandID == id).ToList();
+            return View(ProList);
+        }
+
+        public ActionResult GetAllProducts()
+        {
+            List<Product> ProList = ProductRepository.db.Products.ToList();
+            return View(ProList);
+
+            //yukarıdaki productrep'ten oluşturulan instance direk view methodunun icinde kullanılarak da yapabiliriz.
+            //return View(pr.List().ProcessResult);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            return View(pr.GetObjByID(id).ProcessResult);
         }
     }
 }
